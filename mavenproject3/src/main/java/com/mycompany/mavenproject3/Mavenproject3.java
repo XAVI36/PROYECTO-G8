cbfd68a6c607e845061eb5fcea0436f673a090d1
SAlA-9 <SAlA-9@SI9-9.salas.local>
2024-09-30 09:30:16 -0500

    Primer yayiloncommit

---
+++ mavenproject3/src/main/java/com/mycompany/mavenproject3/Mavenproject3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject3;
+
+/**
+ *
+ * @author SAlA-9
+ */
+public class Mavenproject3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
